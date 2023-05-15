6bf0cd31899ec06737af3bb81e486eaaabb37ebc
SophieBaumbusch <131456352+SophieBaumbusch@users.noreply.github.com>
2023-05-15 17:56:46 +0200

    Added confusion matrix
    Matrix, um die Ergebnisse des ML Models zu veranschaulichen.

--- Diabetes Dataset /DiabetesMLmodel.m
+++ Diabetes Dataset /DiabetesMLmodel.m
@@ -8,18 +8,23 @@ function DiabetesMLmodel()
     
     cv = cvpartition(size(inputs,1),'HoldOut',SplitSize); 
 
-    FeaturesTrain = inputs(cv.training,:); % Select the training data (Parameters) from the table as our features
-    TargetTrain = outputs(cv.training,:); % Select the training data from the Outcome column as our target
-    FeaturesTest = inputs(cv.test,:); % Select the testing data (Parameters) from the table as our features
-    TargetTest = outputs(cv.test,:); % Select the testing data from the Outcome column as our target
+    FeaturesTrain = inputs(cv.training,:); % Select the training data (Parameters) from the table as features
+    TargetTrain = outputs(cv.training,:); % Select the training data from the Outcome column as target
+    FeaturesTest = inputs(cv.test,:); % Select the testing data (Parameters) from the table as features
+    TargetTest = outputs(cv.test,:); % Select the testing data from the Outcome column as target
     
-    Mdl = fitcnet(FeaturesTrain,TargetTrain); % ML Model, classification network
+    MLmodel = fitcnet(FeaturesTrain,TargetTrain); % ML Model, classification network
 
-    trainingLoss = loss(Mdl, FeaturesTrain, TargetTrain); % observe training loss
-    testingLoss = loss(Mdl, FeaturesTest, TargetTest); % observe testing loss
+    [TargetTestPredicted TargetTestScore] = predict(MLmodel,FeaturesTest); % use the trained model on the test set features
+                                                                           % the TargetTestScore is the probability of the class based on the model
+    CnfusionMatrix = confusionmat(TargetTest,TargetTestPredicted); % compute the confusion matrix
+    confusionchart(CnfusionMatrix,unique(TargetTest),'RowSummary','row-normalized'); % show the confusion matrix with class labels
 
-    trainingAccuracy = 1-trainingLoss;
-    testingAccuracy = 1-testingLoss;
+    trainingLoss = loss(MLmodel, FeaturesTrain, TargetTrain); % observe training loss
+    testingLoss = loss(MLmodel, FeaturesTest, TargetTest); % observe testing loss
+
+    trainingAccuracy = 1-trainingLoss; % compute training accuracy 
+    testingAccuracy = 1-testingLoss; % compute testing accuracy 
 
     disp("Training accuracy: " + trainingAccuracy) % output training accuracy
     disp("Testing accuracy: " + testingAccuracy); % output testing accuracy 
