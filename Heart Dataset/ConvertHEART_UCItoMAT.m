e27738167877fd0780b7d0d54b563ea4712c412b
Ricarda Lepsius <Ricarda07@Ricardas-MacBook-Air.local>
2023-05-24 14:24:52 +0200

    second try Heart UCI

--- Heart Dataset/ConvertHEART_UCItoMAT.m
+++ Heart Dataset/ConvertHEART_UCItoMAT.m
@@ -32,6 +32,7 @@ function ConvertHEART_UCItoMAT ()
     while ~feof(curr_file)
 
         curr_line=fgetl(curr_file); %get current line in the file
+<<<<<<< HEAD
         
         %split the read line on all ','; several ,,, are not treates as one
         curr_dat=strsplit(curr_line,',','CollapseDelimiters',false); 
@@ -45,6 +46,15 @@ function ConvertHEART_UCItoMAT ()
             else
                 symptoms_hu(symptoms_hu_index,i) = curr_dat(1,i);
             end
+=======
+
+        curr_dat=strsplit(curr_line,','); %split the read line on all ','
+
+        symptoms_hu_index = symptoms_hu_index + 1; % for each line increase the line in table
+
+        for i = 1:size(curr_dat,2) % insert data from csv file line per line
+            symptoms_hu(symptoms_hu_index,i) = curr_dat(1,i);
+>>>>>>> parent of aa9fed1... Delete ConvertHEART_UCItoMAT.m since it had errors
         end
     end
 
